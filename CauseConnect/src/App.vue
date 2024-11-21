962bf1e2617805af4db59afddd2bc22775e063f8
IVY3135\239006 <planning207@gmail.com>
2024-11-21 09:45:37 +0900

    QandA追加

--- CauseConnect/src/App.vue
+++ CauseConnect/src/App.vue
@@ -4,25 +4,6 @@
 <script setup lang="ts">
 import {ref} from "vue"
 
-const explanation = ref("ここにサイトの説明文入る")
-
-import LoginView from './views/LoginForm/LoginView.vue';
-
-import webFontLoader from 'webfontloader';
-
-export default defineNuxtPlugin( nuxtApp => {
-  webFontLoader.load({
-    google: {
-      families: [
-        'Roboto:100,300,400,500,700,900',
-        'Lato:100,300,400,700,900',
-        'Rubik+Microbe:400',
-        'Patrick+Hand:400',
-        'Noto+Sans+JP:400,700'
-      ],
-    },
-  })
-});
 
 </script>
 
@@ -55,23 +36,4 @@ export default defineNuxtPlugin( nuxtApp => {
 <!-- CSSのコードを書きます。アプリケーションの見た目を整えます。-->
 
 <style>
-.default-font {
-  font-size: xx-large;
-}
-.extra-font {
-  font-family: 'Rubik Microbe';
-  font-size: xx-large;
-}
-.extra-font2 {
-  font-family: 'Patrick Hand';
-  font-size: xx-large;
-}
-.extra-font3 {
-  font-family: 'Lato', sans-serif !important;
-  font-size: xx-large;
-}
-.extra-font4 {
-  font-family: 'Noto Sans JP', sans-serif !important;
-  font-size: xx-large;
-}
 </style>
