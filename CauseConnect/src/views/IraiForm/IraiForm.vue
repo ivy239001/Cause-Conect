75aed28865c48ba070fc00dc024a2749058029fb
LAPTOP-353KICB8\239005 <stoic1214@gmail.com>
2024-11-21 11:45:08 +0900

    依頼項目ページ作成

---
+++ CauseConnect/src/views/IraiForm/IraiForm.vue
@@ -0,0 +1,11 @@
+<script lang="ts">
+import {ref} from "vue"
+
+const inputTextarea = ref("テキストエリアへの入力文字。/n改行も加えてみる。")
+const memberType = ref(1)
+const memberTypeSelect = ref(1)
+const isAgreed = ref(false)
+const isAgreed01 = ref(0)
+const selectedOS = ref([])
+const selectedOSSelect = ref([])
+</script>
