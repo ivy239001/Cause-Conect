2021b71783c121a4f25565e3b34eeead8309902a
IVY3130\239001 <c239001@ivy.ac.jp>
2024-11-26 13:46:36 +0900

    no message

--- CauseConnect/src/axios.js
+++ CauseConnect/src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // ベースURLの設定（Laravelバックエンドが http://localhost:8000 で動作している場合）
-axios.defaults.baseURL = 'http://localhost:8000';  // ここをバックエンドのURLに変更
+axios.defaults.baseURL = 'http://172.16.3.136:8000';  // ここをバックエンドのURLに変更
 
 // ヘッダー設定（必要な場合）
 axios.defaults.headers.common['Content-Type'] = 'application/json';  // JSONデータを送る場合
