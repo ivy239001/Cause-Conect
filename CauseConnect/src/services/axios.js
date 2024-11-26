91942880848680ec9a0b2286811ec1ba80dbeb92
IVY3130\239001 <c239001@ivy.ac.jp>
2024-11-26 11:44:23 +0900

    axios追加

--- CauseConnect/src/services/axios.js
+++ CauseConnect/src/services/axios.js
@@ -1,7 +1,13 @@
-import axios from 'axios';
+import axios from 'axios'
 
-const instance = axios.create({
-  baseURL: '/api',
-});
+// LaravelのAPIエンドポイントを設定
+const apiClient = axios.create({
+  baseURL: 'http://172.16.3.136:8000/api', // Laravel APIのベースURL
+  headers: {
+    'Content-Type': 'application/json',
+  },
+})
 
-export default instance;
+export default apiClient
+
+// 他のAPI通信関数もここに追加可能
