c732285dd3cfe04d0555328b99c3a863e7953ea5
LAPTOP-353KICB8\239005 <stoic1214@gmail.com>
2024-11-27 11:27:34 +0900

    ナビに西暦日付時間をリアルタイム表示するようにしました。

--- CauseConnect/src/main.js
+++ CauseConnect/src/main.js
@@ -2,6 +2,8 @@ import { createApp } from 'vue';
 import App from './App.vue'; // App.vue をインポート
 import router from './router'; // 作成した router をインポート
 import Sarch from '@/components/search.vue'; // 作成した router をインポート
+import './styles/button.css'; // グローバルに読み込む
+import RealTimeClock from './components/RealTimeClock.vue';
 
 createApp(App)
   .use(router) // router を使用
